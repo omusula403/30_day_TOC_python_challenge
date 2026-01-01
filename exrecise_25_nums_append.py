5f462be50943a94098204842c0b6852f28036cb5
Omusula Willikister <omusulawillikister403@gmail.com>
2026-01-01 18:52:00 +0300

    Thur 01 Jan 2026

---
+++ exrecise_25_nums_append.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def main():
+    #Create a List of numbers
+    nums = [1,2,3]
+    #Append 4
+    nums.append(4)
+    print(f"New List = {nums}")
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
